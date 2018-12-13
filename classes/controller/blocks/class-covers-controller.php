5aa78429d9bdbdca232d8e086f0196aa2b44cc22
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2018-12-13 15:40:09 +0530

    PLANET-3008 Removed blocks: Take Action, Campaign Thumbnails and CFC blocks

--- classes/controller/blocks/class-covers-controller.php
+++ classes/controller/blocks/class-covers-controller.php
@@ -103,7 +103,7 @@ if ( ! class_exists( 'Covers_Controller' ) ) {
 				 * See above, to where the the assignment to the $fields variable was made.
 				 */
 				'attrs'         => $fields,
-				'post_type'     => P4BKS_ALLOWED_PAGETYPE,
+				'post_type'     => [''],
 			];
 
 			shortcode_ui_register_for_shortcode( 'shortcake_' . self::BLOCK_NAME, $shortcode_ui_args );
