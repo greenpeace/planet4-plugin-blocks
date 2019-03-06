620d58c953868b2743e21ce2b678da9f26d0d66c
Pablo Cubico <pablocuadrado@gmail.com>
2019-03-06 14:27:42 -0300

    Add max length indicators for fields on the Carousel Header admin.

--- admin/js/blocks-ui.js
+++ admin/js/blocks-ui.js
@@ -659,14 +659,37 @@ var p4_blocks = {
       $('input[data-subheader=true]').closest('.field-block').toggle('full-width-classic' != selected_block_style);
     },
 
+    add_maxlength_with_counter: function(element, maxLength) {
+      $(element).attr('maxlength', maxLength);
+      if (!$(element).next('div').length) {
+        var maxLengthCounter = '<div class="max-length-counter">0/' + maxLength + '</div>';
+        $(maxLengthCounter).insertAfter(element);
+      }
+      $(element).off('input').on('input', function() {
+        var charCount = $(element).val().length;
+        $(element).next('div.max-length-counter').html(charCount + '/' + maxLength);
+      });
+    },
+
     set_maxlength: function() {
+      var me = this;
       var selected_block_style = $('input[name=block_style]:checked').val();
+
       if (selected_block_style == 'full-width-classic') {
-        $('input[name^=\'header_\']').attr('maxlength', 32);
-        $('textarea[name^=\'description_\']').attr('maxlength', 200);
+        $('input[name^=\'header_\']').each(function() {
+          me.add_maxlength_with_counter(this, 32);
+        });
+        $('textarea[name^=\'description_\']').each(function() {
+          me.add_maxlength_with_counter(this, 200);
+        });
+        $('input[name^=\'link_text_\']').each(function() {
+          me.add_maxlength_with_counter(this, 24);
+        })
       } else {
         $('input[name^=\'header_\']').attr('maxlength', 40);
         $('textarea[name^=\'description_\']').removeAttr('maxlength');
+        $('input[name^=\'link_text_\']').removeAttr('maxlength');
+        $('div.maxLengthCounter').remove();
       }
     }
   }
