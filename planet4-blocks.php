42b33def776ff90f4d32003a10c6dc60ab5af020
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-03-29 13:47:30 +0200

    PLANET-3372 Added report for different carousel header styles

--- planet4-blocks.php
+++ planet4-blocks.php
@@ -167,7 +167,7 @@ function plugin_blocks_report() {
 	// phpcs:disable
 	foreach ( $blocks as $block ) {
 		$block = substr( $block, 10 );
-		$shortcode = '%[shortcake_' . $wpdb->esc_like( $block ) . '%';
+		$shortcode = '%[shortcake_' . $wpdb->esc_like( $block ) . ' %';
 		$sql       = $wpdb->prepare(
 			"SELECT ID, post_title
 			FROM `wp_posts` 
@@ -191,6 +191,39 @@ function plugin_blocks_report() {
 			echo '<br>';
 		}
 	}
+
+	// Add to the report a breakdown of different styles for carousel Header
+	$sql       = "SELECT ID, post_title
+						FROM ". $wpdb->prefix . "posts 
+						WHERE post_status = 'publish'
+				      	AND `post_content` REGEXP 'shortcake_carousel_header.*full-width-classic'";
+	$results = $wpdb->get_results( $sql );
+	echo '<hr>';
+	echo '<h2>Carousel Header Full Width Classic style</h2>';
+	foreach ( $results as $result ) {
+		echo '<a href="post.php?post=' . $result->ID . '&action=edit" >' . $result->post_title . '</a>';
+		echo '<br>';
+	}
+
+	// Add to the report a breakdown of different styles for carousel Header
+	// Given that the default (if no style is defined) is the Slide to Gray, include in the query
+	// everything that is not Full Width Classic.
+	$sql       = "SELECT ID, post_title
+						FROM ". $wpdb->prefix . "posts 
+						WHERE post_status = 'publish'
+				      	AND `post_content` REGEXP 'shortcake_carousel_header'
+						AND ID NOT IN (SELECT ID
+											FROM ". $wpdb->prefix . "posts 
+											WHERE post_status = 'publish'
+											AND `post_content` REGEXP 'shortcake_carousel_header.*full-width-classic')";
+	$results = $wpdb->get_results( $sql );
+	echo '<hr>';
+	echo '<h2>Carousel Header Zoom and Slide to Grey</h2>';
+	foreach ( $results as $result ) {
+		echo '<a href="post.php?post=' . $result->ID . '&action=edit" >' . $result->post_title . '</a>';
+		echo '<br>';
+	}
+
 	// phpcs:enable
 }
 
@@ -213,18 +246,36 @@ function plugin_blocks_report_json() {
 		foreach ( $blocks as $block ) {
 
 			$block     = substr( $block, 10 );
-			$shortcode = '%[shortcake_' . $wpdb->esc_like( $block ) . '%';
+			$shortcode = '%[shortcake_' . $wpdb->esc_like( $block ) . ' %';
 			$sql       = $wpdb->prepare(
 				"SELECT count(ID) AS cnt
 				FROM `wp_posts` 
 				WHERE post_status = 'publish' 
 				AND `post_content` LIKE %s", $shortcode );
-
 			$results = $wpdb->get_var( $sql );
 
 			$report[ ucfirst( str_replace( '_', ' ', $block ) ) ] = $results;
 
 		}
+
+		// Add to the report a breakdown of different styles for carousel Header
+		$sql       = "SELECT count(ID) AS cnt
+						FROM ". $wpdb->prefix . "posts 
+						WHERE post_status = 'publish'
+				      	AND `post_content` REGEXP 'shortcake_carousel_header'
+						AND ID NOT IN (SELECT ID
+											FROM ". $wpdb->prefix . "posts 
+											WHERE post_status = 'publish'
+											AND `post_content` REGEXP 'shortcake_carousel_header.*full-width-classic')";
+		$cnt = $wpdb->get_var( $sql );
+		$report['CarouselHeader-Zoom-And-Slide'] = $cnt;
+		$sql       = "SELECT count(ID) AS cnt
+						FROM ". $wpdb->prefix . "posts 
+						WHERE post_status = 'publish'
+				      	AND `post_content` REGEXP 'shortcake_carousel_header.*full-width-classic'";
+		$cnt = $wpdb->get_var( $sql );
+		$report['CarouselHeader-Full-Width-Classic'] = $cnt;
+
 		wp_cache_add( $cache_key, $report, '', 3600 );
 
 	}
