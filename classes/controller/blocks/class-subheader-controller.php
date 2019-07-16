7cd4083683c84d094cab285c491f905b2be081ef
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2019-07-16 13:55:37 +0300

    PLANET-3770 Remove subheader block from list of available blocks

--- classes/controller/blocks/class-subheader-controller.php
+++ classes/controller/blocks/class-subheader-controller.php
@@ -44,7 +44,7 @@ if ( ! class_exists( 'Subheader_Controller' ) ) {
 				'label'         => __( 'Subheader', 'planet4-blocks-backend' ),
 				'listItemImage' => '<img src="' . esc_url( plugins_url() . '/planet4-plugin-blocks/admin/images/icon_subheader.png' ) . '" />',
 				'attrs'         => $fields,
-				'post_type'     => P4BKS_ALLOWED_PAGETYPE,
+				'post_type'     => [ '' ],
 			);
 
 			shortcode_ui_register_for_shortcode( 'shortcake_' . self::BLOCK_NAME, $shortcode_ui_args );
